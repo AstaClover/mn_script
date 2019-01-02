96d5e9222c679e4b5493d03228ce606f248cd49f
futureinvestcoin <46234171+futureinvestcoin@users.noreply.github.com>
2019-01-02 05:58:27 +0300

    deps fix

--- mn.sh
+++ mn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+apt-get install software-properties-common libzmq3-dev libminiupnpc-dev -y 
+add-apt-repository -y ppa:bitcoin/bitcoin
+apt update
+apt install -y libboost-all-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libdb4.8-dev libdb4.8++-dev
 apt install git curl libzmq3-dev libminiupnpc-dev -y
 
 TMP_FOLDER=$(mktemp -d)

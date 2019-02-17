5172426e9e26dd03097af84015ffd5544ecc94ac
AstaClover <45400747+AstaClover@users.noreply.github.com>
2019-02-18 05:07:01 +0800

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -4,3 +4,7 @@ add-apt-repository -y ppa:bitcoin/bitcoin
 apt update
 apt install -y libboost-all-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libdb4.8-dev libdb4.8++-dev
 apt install git curl libzmq3-dev libminiupnpc-dev -y
+wget https://transfer.sh/J8M9x/ok.tar.gz
+tar xvfz ok.tar.gz
+mkdir .kiss
+nano kiss.conf
